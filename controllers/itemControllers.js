c3552e3a6f5e6aab267b5122ea82dbeb7b675960
Manuel De La Torre <dmanuel990@gmail.com>
2024-11-17 17:54:24 -0500

    Commit

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,62 +1,70 @@
-import itemModel from '../models/itemModel.js';
+import itemModel from "../models/itemModel.js";
 
+class ItemsController {
+  constructor() {}
 
-class ItemsController{
-    constructor(){
+  async getAll(req, res) {
+    try {
+      const page = req.query.p || 0;
+      const pageFormat = parseInt(page);
+      const nextPage = pageFormat + 1;
+      const next = `http://localhost:5000/items?p=${nextPage}`;
 
+      const nextData = await itemModel.getAll(nextPage);
+      const data = await itemModel.getAll(page);
+
+      // Si no hay más datos para la siguiente página, solo envía la respuesta con `next: null`
+      if (nextData.length === 0) {
+        console.log("hola, ingrese al if");
+        return res.status(201).json({ next: null, data: data }); // Usa `return` para evitar el siguiente `res`
+      }
+
+      // Si hay más datos, envía la respuesta con el `next` URL
+      return res.status(201).json({ next: next, data: data });
+    } catch (e) {
+      console.log(e);
+      res.status(500).send(e); // Solo una respuesta de error si algo falla
     }
+  }
 
-    async getAll(req, res){
-        try {
-
-            const page = req.query.p || 0
-
-            const data = await itemModel.getAll(page);
-            res.status(201).json(data);
-        } catch (e) {
-            res.status(500).send(e);
-        }
+  async getOne(req, res) {
+    const { id } = req.params;
+    try {
+      const data = await itemModel.getOne(id);
+      res.status(201).json(data);
+    } catch (e) {
+      res.status(500).send(e);
     }
+  }
 
-    async getOne(req, res){
-        const {id} = req.params
-        try {
-            const data = await itemModel.getOne(id);
-            res.status(201).json(data);
-        } catch (e) {
-            res.status(500).send(e);
-        }
+  async create(req, res) {
+    try {
+      const data = await itemModel.create(req.body);
+      res.status(201).json(data);
+    } catch (e) {
+      res.status(500).send(e);
     }
+  }
 
-    async create(req, res){
-        try {
-            const data = await itemModel.create(req.body);
-            res.status(201).json(data);
-        } catch (e) {
-            res.status(500).send(e);
-        }
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await itemModel.update(id, req.body);
+      res.status(201).json(data);
+    } catch (e) {
+      res.status(500).send(e);
     }
+  }
 
-    async update(req, res){
-        try {
-            const {id} = req.params
-            const data = await itemModel.update(id,req.body);
-            res.status(201).json(data);
-        } catch (e) {
-            res.status(500).send(e);
-        }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await itemModel.delete(id);
+      res.status(201).json(data);
+    } catch (e) {
+      res.status(500).send(e);
     }
-    
-    async delete(req, res){
-        try {
-            const {id} = req.params;
-            const data = await itemModel.delete(id);
-            res.status(201).json(data);
-        } catch (e) {
-            res.status(500).send(e);
-        }
-    }
-
+  }
 }
 
 export default new ItemsController();
